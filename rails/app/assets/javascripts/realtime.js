09ba7aba736b965ce8a47beb6139379843f4f29c
Jiehan Zheng <jiehan@Jiehans-MBP.wireless.duke.local>
2014-11-15 14:57:16 -0500

    nodejs

--- rails/app/assets/javascripts/realtime.js
+++ rails/app/assets/javascripts/realtime.js
@@ -1,9 +1,19 @@
-var app = angular.module('tutor', []);
+var app = angular.module('tutor', ['google-maps'.ns()]);
 
 app.controller('LearnController', ['$scope', function($scope) {
-  
+
 }]);
 
 app.controller('MapController', ['$scope', function($scope) {
+  $scope.map = {
+    center: {
+      latitude: 36.001869,
+      longitude: -78.931487
+    },
+    zoom: 14
+  };
+
+  
+
 
 }]);
