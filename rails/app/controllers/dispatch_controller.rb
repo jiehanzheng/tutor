26897f09688906f0965fe1de5986cc520a6e77cf
Jiehan Zheng <zheng@jiehan.org>
2014-11-16 08:22:08 -0500

    versal support

--- rails/app/controllers/dispatch_controller.rb
+++ rails/app/controllers/dispatch_controller.rb
@@ -1,4 +1,10 @@
 class DispatchController < ApplicationController
+  before_filter :allow_iframe_requests, only: [:learn]
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
+
   def learn
   end
 
